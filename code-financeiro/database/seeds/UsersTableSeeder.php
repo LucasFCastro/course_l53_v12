98be529d35575cef31a1cc3192d58c28bd46e113
LucasFCastro <lucasfcastro@yahoo.com.br>
2016-12-28 00:50:53 -0300

    até Compilando sass do admin

---
+++ code-financeiro/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\CodeFin\User::class,1)
+            ->states('admin')
+            ->create([
+                'name' => 'Lucas Castro',
+                'email' => 'admin@user.com'
+            ]);
+
+        factory(\CodeFin\User::class,1)
+            ->create([
+                'name' => 'Client Castro',
+                'email' => 'client@user.com'
+            ]);
+
+    }
+}
---
+++ code-financeiro/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | This file is where you may define all of the routes that are handled
+  | by your application. Just tell Laravel the URIs it should respond
+  | to using a Closure or controller method. Build something great!
+  |
+ */
+
+Route::get('/user2',function(){
+    \Illuminate\Support\Facades\Auth::loginUsingId(2);
+    return "Usuário CLIENT logado!";
+});
+
+Route::get('/user1',function(){
+    \Illuminate\Support\Facades\Auth::loginUsingId(1);
+    return "Usuário ADMIN logado!";
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/home', function(){
+    return redirect()->route('admin.home');
+});
+
+Route::group([
+    'prefix' => 'admin', 
+    'as' => 'admin.'
+    ], function() {
+
+    Auth::routes();
+
+    Route::group(['middleware' => 'can:access-admin'], function(){
+        Route::get('/home', 'HomeController@index')->name('home');
+    });
+
+});
