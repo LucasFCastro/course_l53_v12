effdd118d20be0d147612aa1b17bf585775820ee
LucasFCastro <lucasfcastro@yahoo.com.br>
2017-01-03 08:16:55 -0300

    até a aula: Tratamento de erros de login

--- code-financeiro/app/Http/Controllers/Api/AuthController.php
+++ code-financeiro/app/Http/Controllers/Api/AuthController.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Lang;
 
 class AuthController extends Controller
 {
@@ -18,15 +19,26 @@ class AuthController extends Controller
     {
         $this->validateLogin($request);
 
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
         $credencials = $this->credentials($request);
 
         if ($token = Auth::guard('api')->attempt($credencials)) {
             return $this->sendLoginResponse($request, $token);
         }
+
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
     }
 
     protected function sendLoginResponse(Request $request, $token)
     {
+        $this->clearLoginAttempts($request);
         return response()->json(['token' => $token]);
     }
 
@@ -40,6 +52,34 @@ class AuthController extends Controller
 
         return redirect(env('URL_ADMIN_LOGIN'));
 
+    }
+
+    /**
+     * Get the failed login response instance.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        return response()->json(['message' => Lang::get('auth.failed')], 401);
+    }
+
+    /**
+     * Redirect the user after determining they are locked out.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    protected function sendLockoutResponse(Request $request)
+    {
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+
+        $message = Lang::get('auth.throttle', ['seconds' => $seconds]);
+
+        return response()->json(['message' => $message], 403);
     }
 
 
