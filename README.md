87677f33a508be2196c093a2b0b4f2f1a02d9a94
LucasFCastro <lucasfcastro@yahoo.com.br>
2017-01-11 11:12:19 -0300

    Organizando as pastas

--- README.md
+++
@@ -1,7 +0,0 @@
-npm install -g webpack-dev-server
-npm install -g babel
-npm install -g node-sass
-npm install -g webpack
-npm install
-
-npm start
--- src/js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-require('../sass/app.scss');
-
-// window.$ = window.jQuery = require('jquery');
-require('materialize-css');
-window.Vue = require('vue');
-require('vue-resource');
-require('sweetalert');
-// require('sweetalert.css');
-require('./filters');
